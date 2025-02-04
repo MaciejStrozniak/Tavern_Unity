c5de7e6c6e9e795126ad379ad8d6ad69b1168434
msCDV <macgitcdv@gmail.com>
2025-02-04 11:36:31 +0100

    Initial commit

---
+++ Assets/Scripts/SpellCast.cs
@@ -0,0 +1,76 @@
+using FMOD.Studio;
+using FMODUnity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellCast : MonoBehaviour
+{
+    public GameObject spellPrefab;  // Reference to the spell particle system prefab
+    public Transform spellSpawnPoint;  // The point where the spell is instantiated (e.g., player's hand or camera)
+    private float startOffset;
+    public GameObject spellInstance;
+    public Rigidbody rb;
+    public float spellSpeed; // Speed at which the spell moves
+    public float spellLifetime; // How long the spell lasts before being destroyed
+
+    private AudioSystem audioSystem;
+    private PLAYBACK_STATE pb;
+    private string STOPPED;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startOffset = 2000f;
+        spellSpeed = 200f;
+        spellLifetime = 3f;
+
+        audioSystem = FindObjectOfType<AudioSystem>();
+        audioSystem.SpellSound = RuntimeManager.CreateInstance(audioSystem.spellEvent);
+        STOPPED = "STOPPED";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            audioSystem.SpellSound.getPlaybackState(out pb);
+
+            if (pb.ToString() == STOPPED)
+            {
+                spellCastParticle();
+                audioSystem.SpellCast();
+            }
+
+            spellInstance.transform.position = spellSpawnPoint.position;
+
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            spellShot();
+            audioSystem.SpellRelease();
+        }
+
+        audioSystem.SpellSound.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject.transform));
+    }
+
+    private void spellCastParticle()
+    {
+        // Instantiate the spell at the spawn point
+        Vector3 spawnPosition = spellSpawnPoint.transform.position + spellSpawnPoint.transform.forward * startOffset; // ???
+        spellInstance = Instantiate(spellPrefab, spawnPosition, spellSpawnPoint.rotation);
+
+        // Add velocity to move the spell forward
+        rb = spellInstance.AddComponent<Rigidbody>();
+        rb.useGravity = false;  // Disable gravity so it flies straight        
+    }
+
+    private void spellShot()
+    {        
+        rb.velocity = spellSpawnPoint.forward * spellSpeed;
+
+        // Destroy the spell after a set time (spellLifetime)
+        Destroy(spellInstance, spellLifetime);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# TheTavernAudio_pro
+Small tavern Unity sound implementation project with use of more advanced technics.
